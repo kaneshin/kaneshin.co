9f1bcd02d0e5f732b1520ae9023d4bdca4ca7cb0
Shintaro Kaneko <kaneshin0120@gmail.com>
2020-05-28 00:51:43 +0900

    Add purge section when building tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  purge: [
+    './src/**/*.html',
+    './src/**/*.pug',
+    './src/**/*.s[ac]ss',
+    './src/**/*.css',
+  ],
   theme: {
     fontFamily: {
       display: ['Lato', 'Ubuntu', 'BlinkMacSystemFont', '-apple-system', 'Segoe UI', 'Roboto', 'Oxygen', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', 'Helvetica', 'Arial', 'sans-serif'],
