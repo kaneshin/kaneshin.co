c03b0600c24878f287ed965525650a7be2b1e475
Shintaro Kaneko <kaneshin0120@gmail.com>
2021-02-15 00:32:46 +0900

    :fire: Tidy up

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,55 @@
-/**
- * See ./gatsby directory
- */
-exports.createPages = require("./gatsby/node-create-pages");
-exports.onCreateWebpackConfig = require("./gatsby/node-webpack-config");
+const Promise = require("bluebird");
+const path = require("path");
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions;
+
+  // Create Not Found Page
+  createPage({
+    path: "/404",
+    component: path.resolve("./src/templates/not-found.tsx"),
+  });
+
+  const postsTemplate = path.resolve("./src/templates/posts.tsx");
+
+  return new Promise((resolve, reject) => {
+    resolve(
+      // Create "/posts/:slug"
+      graphql(`
+        {
+          allContentfulPost {
+            edges {
+              node {
+                slug
+              }
+            }
+          }
+        }
+      `).then(result => {
+        if (result.errors) {
+          console.log(result.errors);
+          reject(result.errors);
+        }
+        const edges = result.data.allContentfulPost.edges;
+        edges.forEach(article => {
+          const slug = article.node.slug;
+          createPage({
+            path: path.join("/posts", slug),
+            component: postsTemplate,
+            context: { slug },
+          });
+        });
+      }),
+    );
+  });
+};
+
+exports.onCreateWebpackConfig = ({ stage, getConfig, rules, loaders, actions }) => {
+  const config = {};
+  if (getConfig().mode === "production") {
+    if (stage === "build-javascript") {
+      config["devtool"] = false;
+    }
+  }
+  actions.setWebpackConfig(config);
+};
